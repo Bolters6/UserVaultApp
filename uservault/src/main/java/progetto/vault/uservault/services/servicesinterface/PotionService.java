8e1d7012a8d79ce1c75d0b1e2c2b1ef6a4294691
Bolters6 <96733986+Bolters6@users.noreply.github.com>
2022-02-04 22:31:18 +0100

    Delete uservault directory

--- uservault/src/main/java/progetto/vault/uservault/services/servicesinterface/PotionService.java
+++
@@ -1,9 +0,0 @@
-package progetto.vault.uservault.services.servicesinterface;
-
-import progetto.vault.uservault.models.Potion;
-
-public interface PotionService {
-	Potion createPotion(Potion potion);
-    Potion getPotion(Long id);
-    void deletePotion(Long id);
-}
