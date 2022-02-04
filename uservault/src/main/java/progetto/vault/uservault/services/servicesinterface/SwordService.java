3e7758e2a8b38478d7be1d66a2364fc905d43ef7
Bolters6 <96733986+Bolters6@users.noreply.github.com>
2022-02-04 20:46:25 +0100

    Delete uservault directory

--- uservault/src/main/java/progetto/vault/uservault/services/servicesinterface/SwordService.java
+++
@@ -1,9 +0,0 @@
-package progetto.vault.uservault.services.servicesinterface;
-
-import progetto.vault.uservault.models.Sword;
-
-public interface SwordService {
-    Sword createSword(Sword sword);
-    Sword getSword(Long id);
-    void deleteSword(Long id);
-}
