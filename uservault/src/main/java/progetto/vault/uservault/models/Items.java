121333951967e213c90748931d30ce78d81bc128
Bolters6 <96733986+Bolters6@users.noreply.github.com>
2022-02-04 19:56:53 +0100

    Add files via upload

--- uservault/src/main/java/progetto/vault/uservault/models/Items.java
+++ uservault/src/main/java/progetto/vault/uservault/models/Items.java
@@ -1,16 +1,23 @@
 package progetto.vault.uservault.models;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+
 import java.util.List;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
+
+@JsonInclude(NON_NULL)
 @AllArgsConstructor
 @Data
 public class Items {
     private Integer bless;
     private Integer souls;
     private Integer denaro;
+    @JsonInclude(NON_EMPTY)
     private List<Potion> potions;
+    @JsonInclude(NON_EMPTY)
     private List<Sword> swords;
     private String wings;
 }
